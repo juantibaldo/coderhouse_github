9451ce467082ae9978b7bcce579447bb522abd2c
juantibaldo <juantibaldo@gmail.com>
2021-03-22 20:33:19 -0300

    agrego beautify al codigo

--- javascript.js
+++ javascript.js
@@ -1,29 +1,26 @@
 function showDate() {
-    var myDate = document.getElementById("currentDate");
-  
-    var date = new Date();
-    var monthNames = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December"
-    ];
-    
-    var mm = monthNames[date.getMonth()];
-    var today = `${mm} ${date.getDate()}, ${date.getFullYear()}`;
-  
-    myDate.innerText = `${today}`;
-  }
-  
-  setInterval(showDate, 1000);
-  
+  var myDate = document.getElementById("currentDate");
 
- 
+  var date = new Date();
+  var monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  var mm = monthNames[date.getMonth()];
+  var today = `${mm} ${date.getDate()}, ${date.getFullYear()}`;
+
+  myDate.innerText = `${today}`;
+}
+
+setInterval(showDate, 1000);
